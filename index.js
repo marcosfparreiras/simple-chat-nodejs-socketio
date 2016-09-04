963ed4ff41fa384330eb2d6c2ec56c1b62558d32
Marcos Parreiras <marcosfparreiras@gmail.com>
2016-09-04 12:07:07 -0300

    Broadcast message on login/logout; Add user identifying

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  socket.broadcast.emit('chat message', 'user joined the room');
   socket.on('chat message', function(msg){
     socket.broadcast.emit('chat message', msg);
   });
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    socket.broadcast.emit('chat message', 'user left the room');
   });
 });
-
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');

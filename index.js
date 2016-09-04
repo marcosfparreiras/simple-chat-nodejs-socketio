fbb5fadb34aa12f8a9d99b16046f0cf3d3097f9d
Marcos Parreiras <marcosfparreiras@gmail.com>
2016-09-04 11:02:02 -0300

    Add console logging for connection and disconnection

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  console.log('a user connected');
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    socket.broadcast.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
 });
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
